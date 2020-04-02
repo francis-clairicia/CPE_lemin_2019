7c5b1e1eb5532e061a1a98a72a3ee90a3b4580d5
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-04-02 23:06:02 +0200

    [FIX] Malloc error handling in room creation

--- src/room.c
+++ src/room.c
@@ -15,10 +15,14 @@ room_t *create_room(char const *name, int x, int y)
     if (room != NULL) {
         room->linked = NULL;
         room->name = my_strdup(name);
+        if (room->name == NULL) {
+            free(room);
+            return (NULL);
+        }
         room->start = false;
+        room->end = false;
         room->pos.x = x;
         room->pos.y = y;
-        room->end = false;
         room->ants = NULL;
     }
     return (room);
@@ -26,6 +30,8 @@ room_t *create_room(char const *name, int x, int y)
 
 void destroy_room(room_t *room)
 {
+    if (room == NULL)
+        return;
     my_free_list(&room->linked, NULL);
     my_free_list(&room->ants, NULL);
     free(room->name);
