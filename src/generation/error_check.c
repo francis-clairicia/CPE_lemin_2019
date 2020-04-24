dbe6b9d30d399e5b0a1749d74a858230be336d45
pmatheo <matheo.pons@epitech.eu>
2020-04-24 20:55:00 +0200

    <ADD> No way between start and end error check
    Co-authored-by: Etienne Delrieux <etienne1.delrieux@epitech.eu>
    Co-authored-by: francis.clairicia-rose-claire-josephine@epitech.eu <francis.clairicia-rose-claire-josephine@epitech.eu>

--- src/generation/error_check.c
+++ src/generation/error_check.c
@@ -20,13 +20,46 @@ static bool basic_requierement(anthill_t *anthill)
     return (true);
 }
 
+static bool room_in_list(list_t *list, room_t *room)
+{
+    while (list != NULL) {
+        if (NODE_DATA(list, room_t *) == room)
+            return (true);
+        list = list->next;
+    }
+    return (false);
+}
+
+static bool check_way_to_end(room_t *room_to_check, list_t **rooms_visited)
+{
+    room_t *room = NULL;
+    list_t *node = NULL;
+
+    if (room_to_check->end)
+        return (true);
+    for (node = room_to_check->linked; node != NULL; node = node->next) {
+        room = NODE_DATA(node, room_t *);
+        if (room_in_list(*rooms_visited, room))
+            continue;
+        MY_APPEND_TO_LIST(rooms_visited, room);
+        if (check_way_to_end(room, rooms_visited) == true)
+            return (true);
+    }
+    return (false);
+}
+
 bool error_generation(anthill_t *anthill)
 {
+    list_t *rooms_visited = NULL;
+    bool at_least_one_way_to_end = true;
+
     if (anthill == NULL || basic_requierement(anthill) == false)
         return (false);
     if (my_list_size(anthill->start->linked) == 0)
         return (false);
     if (my_list_size(anthill->end->linked) == 0)
         return (false);
-    return (true);
+    at_least_one_way_to_end = check_way_to_end(anthill->start, &rooms_visited);
+    my_free_list(&rooms_visited, NULL);
+    return (at_least_one_way_to_end);
 }
